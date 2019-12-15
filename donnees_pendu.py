5849924d5474a43619c9f58a36b319c6ca16bf8e
Newton-Stone <58741715+Newton-Stone@users.noreply.github.com>
2019-12-15 17:55:03 +0100

    Jeu du pendu
    Jeu_pendu pour jouer, fonctions et données sont 2 modules pour le faire fonctionner

---
+++ donnees_pendu.py
@@ -0,0 +1,50 @@
+# -*-coding:Latin-1 -*
+import os # On importe le module os 
+
+"""Ce fichier définit quelques données, sous la forme de variables,
+utiles au programme pendu"""
+
+# Nombre de coups par partie
+nb_coups = 10
+
+# Nom du fichier stockant les scores
+nom_fichier_scores = "scores"
+
+# Liste des mots du pendu
+liste_mots = [
+    "armoire",
+    "boucle",
+    "buisson",
+    "bureau",
+    "chaise",
+    "carton",
+    "couteau",
+    "fichier",
+    "garage",
+    "glace",
+    "journal",
+    "kiwi",
+    "lampe",
+    "liste",
+    "montagne",
+    "remise",
+    "sandale",
+    "taxi",
+    "vampire", 
+    "volant", 
+    "anticonstitutionnellement", 
+    "mithridatisation", 
+    "propÃ©deutique", 
+    "lui", 
+    "azur", 
+    "dent", 
+    "bidule", 
+    "truc", 
+    "astuce", 
+    "puzzle", 
+    "coyote", 
+    "python", 
+    "simplon",
+]
+
+os.system("pause")
